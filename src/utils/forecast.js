436d8b3294a3fd808c3ce51cec65c557c707e60e
lernest <19194046+lernest@users.noreply.github.com>
2020-03-26 19:51:23 -0700

    added daily high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,12 @@
 const request = require("request");
 
+// in daily.data
+/* 
+"temperatureHigh": 56.53,
+"temperatureHighTime": 1585259400,
+"temperatureLow": 45.48,
+"temperatureLowTime": 1585317600, */
+
 const forecast = (lat, long, callback) => {
   const url =
     "https://api.darksky.net/forecast/12502acaa2e0eb7327163f97ca203d8e/" +
@@ -20,7 +27,11 @@ const forecast = (lat, long, callback) => {
         data.temperature +
         " degrees out.  There is a " +
         data.precipProbability * 100 +
-        "% chance of rain";
+        "% chance of rain.  The high for the day is " +
+        body.daily.data[0].temperatureHigh +
+        " degrees, and the low is " +
+        body.daily.data[0].temperatureLow +
+        " degrees.";
       callback(undefined, forecast_str);
     }
   });
